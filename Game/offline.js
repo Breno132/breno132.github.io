10c17509552ae8e486b2778ba93308482b8c24f1
Breno132 <43450646+Breno132@users.noreply.github.com>
2019-05-08 10:43:41 -0300

    Add files via upload

---
+++ Game/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1557322936,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/staeshipcontrole-sheet0.png",
+		"images/botão-sheet0.png",
+		"images/botão-sheet1.png",
+		"images/bala-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

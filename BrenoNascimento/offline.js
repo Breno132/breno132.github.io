69dc9308332dc7b97184c23e1903e982a782580f
Breno132 <43450646+Breno132@users.noreply.github.com>
2019-05-22 10:13:50 -0300

    Add files via upload

---
+++ BrenoNascimento/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1558530408,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/staeshipcontrole-sheet0.png",
+		"images/botão-sheet0.png",
+		"images/botão-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/direita-sheet0.png",
+		"images/direita-sheet1.png",
+		"images/esquerda-sheet0.png",
+		"images/esquerda-sheet1.png",
+		"images/cima-sheet0.png",
+		"images/cima-sheet1.png",
+		"images/baixo-sheet0.png",
+		"images/baixo-sheet1.png",
+		"images/inimigo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
